8a1ec956c2ae435ec8227a058c37f5d886a18cb6
GabrielBatavia <143175487+GabrielBatavia@users.noreply.github.com>
2024-09-21 21:54:21 +0700

    Update Artikel.java

--- Artikel.java
+++ Artikel.java
@@ -1,3 +1,33 @@
 public class Artikel {
-    
+    private String judulArtikel;
+    private String isiArtikel;
+
+    // Constructor
+    public Artikel(String judulArtikel, String isiArtikel) {
+        this.judulArtikel = judulArtikel;
+        this.isiArtikel = isiArtikel;
+    }
+
+    // Method untuk membaca artikel
+    public void bacaArtikel() {
+        System.out.println("Judul Artikel: " + judulArtikel);
+        System.out.println("Isi Artikel: \n" + isiArtikel);
+    }
+
+    // Getter dan Setter
+    public String getJudulArtikel() {
+        return judulArtikel;
+    }
+
+    public void setJudulArtikel(String judulArtikel) {
+        this.judulArtikel = judulArtikel;
+    }
+
+    public String getIsiArtikel() {
+        return isiArtikel;
+    }
+
+    public void setIsiArtikel(String isiArtikel) {
+        this.isiArtikel = isiArtikel;
+    }
 }
