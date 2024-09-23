2753cd2cb2b6f9c27995e6599ca3370a97670b8c
aslamrosul <nvrsrndrmrx@gmail.com>
2024-09-23 11:15:02 +0700

    add instruktur fungsi

--- DataBase.java
+++ DataBase.java
@@ -5,13 +5,21 @@ public class DataBase {
     private Instruktur instruktur[] = new Instruktur[0];
     private Kursus kursus[] = new Kursus[0];
 
-    void tambahInstruktur(String nama, String email) {
+    void tambahInstruktur(String nama, String mapel) {
         Instruktur temp[] = new Instruktur[instruktur.length + 1];
         for (int i = 0; i < instruktur.length; i++) {
             temp[i] = instruktur[i];
         }
         instruktur = temp;
-        instruktur[instruktur.length - 1] = new Instruktur();
-        instruktur[instruktur.length - 1].setInstruktur(nama, email);
+        instruktur[instruktur.length - 1] = new Instruktur(nama,mapel);
     }
+
+    void lihatInstruktur(){
+        for (int i = 0; i < instruktur.length; i++) {
+            System.out.println("Nama : " + instruktur[i].getNamaInstruktur());
+            System.out.println("Mata Pelajaran : "+ instruktur[i].getMatapelajaran());
+        }
+        
+    }
+
 }
