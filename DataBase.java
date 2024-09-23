3b1a919d6e58c1ad8beaa26bf7ef79ecd2979b24
Ruphasa <rfrizqifauzan@gmail.com>
2024-09-23 09:49:12 +0700

    tidak kesave ygy

--- DataBase.java
+++ DataBase.java
@@ -1,45 +1,17 @@
 import java.util.Scanner;
 
-public class Database {
-    Instruktur instruktur[]=new Instruktur[0];
-    Kursus kursus[]=new Kursus[0];
+public class DataBase {
+    private Peserta Peserta[] = new Peserta[0];
+    private Instruktur instruktur[] = new Instruktur[0];
+    private Kursus kursus[] = new Kursus[0];
 
-    void tambahKontent(String matapelajaran, String bentukKontent){
-        Scanner sc = new Scanner(System.in);
-        if (matapelajaran.equals(temp.mataPelajaran)) {
-            if (bentukKontent.equals("materi")) {
-                    temp.materiPembelajaran = new MateriPembelajaran();
-                }else if (bentukKontent.equals("video")) {
-                    System.out.println("Masukkan judul video: ");
-                    String judulVideo = sc.nextLine();
-                    System.out.println("Masukkan URL video: ");
-                    String urlVideo = sc.nextLine();
-                    System.out.println("Masukkan durasi video (dalam detik): ");
-                    int durasi = sc.nextInt();
-                    sc.nextLine();
-                    temp.video = new Video(judulVideo, urlVideo, durasi);
-                }else if (bentukKontent.equals("artikel")) {
-                    System.out.println("Masukkan judul artikel: ");
-                    String judulArtikel = sc.nextLine();
-                    System.out.println("Masukkan isi artikel: ");
-                    String isiArtikel = sc.nextLine();
-                    temp.artikel = new Artikel(judulArtikel, isiArtikel);
-                }else if (bentukKontent.equals("kuis")) {
-                    System.out.println("Masukkan judul kuis: ");
-                    String judulKuis = sc.nextLine();
-                    System.out.println("Masukkan banyak pertanyaan: ");
-                    int banyakPertanyaan = sc.nextInt();
-                    sc.nextLine();
-                    String[] pertanyaan = new String[banyakPertanyaan-1];
-                    String[] jawaban = new String[banyakPertanyaan-1];
-                    for (int i = 0; i < banyakPertanyaan; i++) {
-                        System.out.println("Masukkan pertanyaan " + (i+1) + ": ");
-                        pertanyaan[i] = sc.nextLine();
-                        System.out.println("Masukkan jawaban " + (i+1) + ": ");
-                        jawaban[i] = sc.nextLine();
-                    }
-                    temp.kuis = new Kuis(judulKuis, pertanyaan, jawaban);
-                }
-            }
+    void tambahInstruktur(String nama, String email) {
+        Instruktur temp[] = new Instruktur[instruktur.length + 1];
+        for (int i = 0; i < instruktur.length; i++) {
+            temp[i] = instruktur[i];
+        }
+        instruktur = temp;
+        instruktur[instruktur.length - 1] = new Instruktur();
+        instruktur[instruktur.length - 1].setInstruktur(nama, email);
     }
 }
