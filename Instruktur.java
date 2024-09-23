fd825142de16044df8f9545e1b8503731144a877
aslamrosul <nvrsrndrmrx@gmail.com>
2024-09-23 10:17:49 +0700

    update instruktur

--- Instruktur.java
+++ Instruktur.java
@@ -1,3 +1,26 @@
 public class Instruktur {
-    
+    private DataBase db = new DataBase();
+    private String namaInstruktur;
+    private String matapelajaran;
+
+    public Instruktur(String nama, String mapel) {
+        namaInstruktur = nama;
+        matapelajaran = mapel;
+    }
+
+    public void setNamaInstruktur(String nama) {
+        namaInstruktur = nama;
+    }
+
+    public String getNamaInstruktur() {
+        return namaInstruktur;
+    }
+
+    public void setMatapelajaran(String mapel) {
+        matapelajaran = mapel;
+    }
+
+    public String getMatapelajaran() {
+        return matapelajaran;
+    }
 }
